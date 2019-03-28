26480e1b3fa5c1f0fba0883c6b914dcd311ed288
Sercurio <penalva.louis@gmail.com>
2019-03-28 14:26:48 +0100

    ajout des pdfs des days

--- j03/ex00/ft_ft.c
+++ j03/ex00/ft_ft.c
@@ -1,10 +1,15 @@
 #include <unistd.h>
 
+void ft_ft(int*);
+
 int main(void){
-    ft_ft();
+    int nbr = 0;
+    int *ptr = &nbr;
+
+    ft_ft(ptr);
     return 0;
 }
 
-void ft_ft(void){
-
+void ft_ft(int *nbr){
+    *nbr = 42;
 }
---
+++ j03/ex01/ft_ultimate_ft.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_ultimate_ft(int*********);
+
+int main(void){
+    int nbr = 0;
+    int *********ptr = nbr;
+
+    ft_ultimate_ft(ptr);
+    printf("%d", nbr);
+
+    return 0;
+}
+
+void ft_ultimate_ft(int *********nbr){
+    *********nbr = 42;
+}
