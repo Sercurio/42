31bcb46f43e5993b24ffd559bc28602bc15afe9c
Sercurio <penalva.louis@gmail.com>
2019-03-28 11:33:50 +0100

    ex07 amelioré

--- j02/ex07/ft_print_combn.c
+++ j02/ex07/ft_print_combn.c
@@ -5,69 +5,53 @@ void ft_print_combn(int);
 void ft_putnbr(int);
 
 int main(void){
-    ft_print_combn(4);
+    ft_print_combn(8);
     return 0;
 }
 
 void ft_print_combn(int n){
-    char tab[12];
-    tab[11] = '\0';
-    int i = 10;
-    int min_i = (i) - n;
-    int max_i = 10;
-    int temp= min_i;
+    char tab[n + 1];
+    int i = n - 1;
     int nbr = 0;
     int repetition = 0;
-    int mod;
+    int temp;
+
+    tab[n] = '\0';
 
     int limites[11]={0,9,89,789,6789,56789,456789,3456789,23456789,123456789};
     int limite = limites[n];
 
-    i = min_i;
-    int j=0;
-    while(tab[i] != '\0'){
-        tab[i++] = j++ + '0';
-        nbr += 
+    while(i >= 0){
+        tab[i] = i + '0';
+        i--;
     }
-    j = min_i;
 
-
-    while(tab[j] != '\0'){
-        ft_putchar(tab[j++]);
+    i = 0;
+    while(i < n - 1){
+        nbr += tab[i] - 48;
+        nbr *= 10;
+        i++;
     }
-    /*
-    nbr = j - 48;
-    temp = nbr;
-    ft_putchar(nbr + '0');
+
     while(nbr <= limite){
-        while(temp != 0 && mod != 0){
-            mod = temp % 10 + '0';
-            tab[i--] = mod;
+        temp = nbr;
+        repetition = 0;
+        while(temp != 0){
+            tab[i--] = temp % 10 + '0';
             temp /= 10;
-            ft_putchar(tab[i]);
-            ft_putchar('\n');
         }
-        i = min_i;
-        temp = tab[i];
-        while(tab[i++] != '\0'){
-            if(temp > tab[i])
+        while(i++ < n){
+            if(tab[i] >= tab[i + 1])
                 repetition = 1;
-            else
-                repetition = 0;
         }
         if(!repetition){
-            i = min_i;
-            while(tab[i] != '\0'){
-                ft_putchar(tab[i++]);
-                ft_putchar('\n');
-            }
+            ft_putnbr(nbr);
+            ft_putchar('\n');
+            compteur++;
         }
-        i=max_i;
         nbr++;
-    }*/
+    }
 }
-
-
 
 void ft_putnbr(int n){
     char tab[12];
